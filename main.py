4f03073f10e245ab7e723552cd1df01c07236ca7
tamakiii <tamakiii@users.noreply.github.com>
2023-11-29 19:54:37 +0900

    pipe to whisper

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 import sys
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from datasets import load_dataset
 
 def main():
     if len(sys.argv) < 2:
         print("Usage: python script.py <path>")
         sys.exit(1)
 
-    path = sys.argv[1]
-    print(path)
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
+    model_id = "openai/whisper-large-v3"
+
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    )
+    model.to(device)
+
+    processor = AutoProcessor.from_pretrained(model_id)
+
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        max_new_tokens=128,
+        chunk_length_s=30,
+        batch_size=16,
+        return_timestamps=True,
+        torch_dtype=torch_dtype,
+        device=device,
+    )
+
+    dataset = load_dataset("distil-whisper/librispeech_long", "clean", split="validation")
+    sample = dataset[0]["audio"]
+
+    result = pipe(sys.argv[1])
+    print(result["text"])
 
 if __name__ == "__main__":
     main()
